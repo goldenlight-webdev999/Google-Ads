19761f8cb1ad3e41030fa7f7d1d8b733fba46e63
goldenlightwebdev <goldenlight.webdev999@gmail.com>
2019-05-10 16:52:36 -0400

    Upload works

---
+++ custom-html-animation/2-728x90/script.js
@@ -0,0 +1,11 @@
+var params = {
+	container: document.getElementById('lottie'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	animationData: animationData
+};
+
+var anim;
+
+anim = lottie.loadAnimation(params);
